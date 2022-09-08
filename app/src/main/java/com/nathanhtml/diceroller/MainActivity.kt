fa1b889b8dcf8e0a3b7fd82ad06aeda5b2d9bf0d
Nathan Flacher <nathan.flacher.pro@gmail.com>
2022-09-08 18:58:11 +0200

    add dice images

--- app/src/main/java/com/nathanhtml/diceroller/MainActivity.kt
+++ app/src/main/java/com/nathanhtml/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.nathanhtml.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import com.nathanhtml.diceroller.model.Dice
@@ -14,12 +15,19 @@ class MainActivity : AppCompatActivity() {
 
         // Dice process
         val dice : Dice = Dice(6);
-        val rollText : TextView = findViewById(R.id.print_roll);
+        val diceImage : ImageView = findViewById(R.id.print_roll);
         val rollButton : Button = findViewById(R.id.btn_roll);
-        val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+        var result : Int = dice.roll();
+        val toast = Toast.makeText(this, "You get $result", Toast.LENGTH_SHORT)
         rollButton.setOnClickListener{
+            result = dice.roll();
+            if (result == 1) diceImage.setImageResource(R.drawable.dice_1);
+            else if (result == 2) diceImage.setImageResource(R.drawable.dice_2);
+            else if (result == 3) diceImage.setImageResource(R.drawable.dice_3);
+            else if (result == 4) diceImage.setImageResource(R.drawable.dice_4);
+            else if (result == 5) diceImage.setImageResource(R.drawable.dice_5);
+            else if (result == 6) diceImage.setImageResource(R.drawable.dice_6);
             toast.show();
-            rollText.text = dice.roll().toString();
         }
     }
 }
